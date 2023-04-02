dce201e905ab4ebad58ca5b8ce109713fefd9dfe
vuhoob <phamvuhop1812@gmail.com>
2023-04-02 23:08:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/plant_signup_fpass/forgotPass.kt
@@ -0,0 +1,11 @@
+package com.example.plant_signup_fpass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class forgotPass : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_pass)
+    }
+}
